4c266174d5c80ef67719be519a7672e10c223512
Jon Dough <49297268+jetstream0@users.noreply.github.com>
2023-04-24 18:28:54 -0700

    Add faucet to bot (#1)
    tested but not completely certain

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ const songbird = require("./songbird.js");
 const chart = require("./chart.js");
 const util = require("./util.js");
 const keep_alive = require("./keep_alive.js");
+const { fetch } = require('cross-fetch');
+const fs = require('fs');
+
+//why do we have 3 http request libraries? why, good question!
+//todo: switch to one and remove the others
 
 const client = new discord.Client({
   intents: [discord.GatewayIntentBits.Guilds]
 });
 
 const ADMINS = ["239770148305764352", "288612712680914954", "875942059503149066", "600071769721929746", "1074092955943571497"];
+
+//23 1/2 hours
+const CLAIM_FREQ = 23.5*60*60*1000;
+const MAX_CLAIMS_PER_MONTH = 11111;
+const HOLDING_REQUIREMENT = 2000;
 
 let historic_data_cache;
 let liqudity_cache;
@@ -23,7 +33,7 @@ client.once('ready', async (info) => {
     let price;
     try {
       historic_data_cache = await songbird.get_historic();
-      price = String(historic_data_cache.ohlcv_list.slice(-1)[0][4]).slice(0, 10);
+      price = (await songbird.get_price()).base_token_price_usd.slice(0, 10);
       //also get sgb price
       sgb_price_cache = await songbird.get_coin_price("songbird");
       //liquidity
@@ -59,7 +69,7 @@ client.once('ready', async (info) => {
   }
   setTimeout(async () => {
     await db.milestone_check(send_announcement);
-  }, 2500);
+  }, 7500);
   setInterval(async () => {
     await db.milestone_check(send_announcement);
   }, 30*60*1000);
@@ -81,6 +91,10 @@ client.on('interactionCreate', async interaction => {
       {
         name: "/help",
         value: "Get a list of commands."
+      },
+      {
+        name: "/faucet",
+        value: "Use the monthly XAC faucet, and participate in the XAC distribution!"
       },
       {
         name: "/price",
@@ -110,7 +124,11 @@ client.on('interactionCreate', async interaction => {
       admin_embed.addFields([
         {
           name: "/send",
-          value: "Admins can send XAC to discord users or addresses"
+          value: "Admins can send XAC to discord users or addresses."
+        },
+        {
+          name: "/change_register",
+          value: "Admins can change a registered user's address."
         }
       ]);
       admin_embed.setFooter({ text: "\"The ships hung in the sky in much the same way that bricks don't.\" -Douglas Adams" });
@@ -174,7 +192,11 @@ client.on('interactionCreate', async interaction => {
       {
         name: "BlazeSwap Liquidity",
         value: "$"+util.format_commas(String(liqudity_cache))+"~"
-      }
+      },
+      {
+        name: "Pangolin",
+        value: "[Pool](https://app.pangolin.exchange/#/swap?outputCurrency=0x61b64c643fccd6ff34fc58c8ddff4579a89e2723) | [GeckoTerminal](https://www.geckoterminal.com/songbird/pools/0xdd06d19b1217423ba474783a16e4a9798b794225)"
+      },
     ]);
     pools_embed.setFooter({ text: "Made by prussia.dev" });
     return await interaction.reply({ embeds: [pools_embed] });
@@ -263,13 +285,50 @@ client.on('interactionCreate', async interaction => {
     if (!address_valid) {
       return await interaction.editReply(`Invalid address \`${address}\` provided`);
     }
-    await db.register_user(interaction.user.id, address);
+    let register = await db.register_user(interaction.user.id, address, false);
+    if (!register) {
+      return await interaction.editReply("You have already registered an address! Contact an admin if it needs to be changed.");
+    }
     let register_embed = new discord.EmbedBuilder();
     register_embed.setTitle("Successfully Registered!");
     register_embed.setColor("#7ed11f");
     register_embed.setDescription("Thanks for registering! Now admins can send XAC to you if you win a prize, or tip you.");
     register_embed.setFooter({ text: "Made by prussia.dev" });
     return await interaction.editReply({ embeds: [register_embed] });
+  } else if (command === "faucet") {
+    await interaction.deferReply();
+    if (interaction.channel?.id !== "1087903395962179646") {
+      return await interaction.editReply("Failed, cannot use this command outside of the faucet claims channel.");
+    }
+    //make sure they are older than 1 hour old in server
+    if (interaction.member.joinedTimestamp+(60*60*1000) > Date.now()) {
+      return await interaction.editReply("You joined the server in the last hour, try again after you've been in the server for 1 hour. Check out the announcements or talk or something.");
+    }
+    //make sure they are registered
+    let user_info = await db.get_user(user.id);
+    if (!user_info) {
+      return await interaction.editReply("Failed, please `/register` your address with the bot before using faucet.");
+    }
+    //send captcha and modal thing with id set to code and nonce
+    let captcha_info = await util.get_text_captcha();
+    if (!captcha_info) {
+      return await interaction.editReply("Error, captcha probably currently down. Wait a bit and/or notify admins.");
+    }
+    //embed
+    let captcha_embed = new discord.EmbedBuilder();
+    captcha_embed.setTitle("One more step...");
+    captcha_embed.setColor("#2c16f7");
+    captcha_embed.setDescription("Please answer the captcha before you claim your XAC!")
+    captcha_embed.setImage(captcha_info.challenge_url);
+    captcha_embed.setFooter({ text: "Almost there!" });
+    //send button that opens modal
+    let captcha_button = new discord.ButtonBuilder()
+      .setCustomId("capbtn-"+captcha_info.challenge_code+"-"+captcha_info.challenge_nonce+"-"+user.id+"-"+String(Date.now()))
+      .setLabel("Claim Faucet")
+      .setStyle('Primary');
+    let action_row = new discord.ActionRowBuilder();
+    action_row.addComponents(captcha_button);
+    return await interaction.editReply({ embeds: [captcha_embed], components: [action_row] })
   }
 
   //admin command
@@ -327,7 +386,153 @@ client.on('interactionCreate', async interaction => {
       send_embed.setDescription(`${String(amount)} XAC sent to ${receiver}. [View tx](https://songbird-explorer.flare.network/tx/${tx}).`);
       send_embed.setFooter({ text: "Made by prussia.dev" });
       return await interaction.editReply({ embeds: [send_embed] });
+    } else if (command === "change_register") {
+      await interaction.deferReply();
+      let address = (await params.get("address")).value;
+      let target = (await params.get("target")).user;
+      //validate address
+      let address_valid;
+      try {
+        address_valid = songbird.is_valid(address);
+      } catch (e) {
+        address_valid = false;
+      }
+      if (!address_valid) {
+        return interaction.editReply("Failed, invalid address");
+      }
+      //make sure target is registered
+      let user_info = await db.get_user(target.id);
+      if (!user_info) {
+        return await interaction.editReply("Failed, target user has not registered with bot.");
+      }
+      //change
+      await db.register_user(target.id, address, true);
+      //success
+      return await interaction.editReply("Successfully changed user's address (admin only action).");
     }
+  }
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isButton() && !interaction.isModalSubmit()) return;
+
+  let customId = interaction.customId;
+  let user = interaction.user;
+  if (customId.startsWith("capbtn-")) {
+    let og_user = customId.split("-")[3];
+    if (user.id !== og_user) {
+      return await interaction.reply({ ephemeral: true, content: "You cannot claim for someone else! Run the `/faucet` command yourself to claim." })
+    }
+    let created_date = Number(customId.split("-")[4]);
+    //if they take more than 2 minutes to respond
+    if (created_date+(2*60*1000) < Date.now()) {
+      return await interaction.reply("Failed, took too long to answer captcha. Run `/faucet` again.");
+    }
+    //button that should open modal
+    let modal = new discord.ModalBuilder()
+      .setCustomId(customId.replace("capbtn-", "capmod-"))
+      .setTitle('Faucet Captcha');
+    let captcha_answer_input = new discord.TextInputBuilder()
+      .setCustomId("answer")
+      .setStyle(discord.TextInputStyle.Short)
+      .setMaxLength(10)
+      .setLabel("What are the characters in the captcha?")
+      .setRequired(true);
+    let action_row = new discord.ActionRowBuilder();
+    action_row.addComponents(captcha_answer_input);
+    modal.addComponents(action_row);
+    return await interaction.showModal(modal);
+  } else if (customId.startsWith("capmod-")) {
+    //modal
+    await interaction.deferReply();
+    //make sure they are registered
+    let user_info = await db.get_user(user.id);
+    if (!user_info) return interaction.editReply("Failed, you are not registered.");
+    //edit prev message to disable button
+    try {
+      let captcha_button = new discord.ButtonBuilder()
+        .setCustomId("capbtn-disabled")
+        .setLabel("Claim Faucet")
+        .setDisabled(true)
+        .setStyle('Primary');
+      let action_row = new discord.ActionRowBuilder();
+      action_row.addComponents(captcha_button);
+      await interaction.channel.fetch();
+      await interaction.message.edit({ embeds: interaction.message.embeds, components: [action_row] });
+    } catch (e) {
+      console.log(e);
+    }
+    //get all needed info
+    //since address is taken from registered address, that means one address per discord user
+    let address = user_info.address;
+    let code = customId.split("-")[1];
+    let nonce = customId.split("-")[2];
+    let answer = interaction.fields.getTextInputValue("answer");
+    //verify captcha
+    let passed_captcha = await util.verify_text_captcha(code, nonce, answer);
+    if (!passed_captcha) {
+      return await interaction.editReply("Error, you failed captcha. Try again.");
+    }
+    //make sure claim limit not already exceeded
+    let claims_month = await db.get_claims_this_month();
+    if (claims_month >= MAX_CLAIMS_PER_MONTH) {
+      return await interaction.editReply(`We already reached this month's max claim limit (${claims_month})!`);
+    }
+    //make sure not blacklisted
+    try {
+      let blacklist = fs.readFileSync("blacklist.txt", "utf-8").split("\n").map((item) => item.trim().toLowerCase());
+      if (blacklist.includes(address)) {
+        return await interaction.editReply("Your address has been blacklisted. Contact the admins if you think this is a mistake. If this isn't a mistake, contact us anyways! We'd love to talk with you ;)");
+      }
+    } catch (e) {
+      //probably blacklist.txt does not exist
+      //console.log(e);
+    }
+    //make sure they aren't claiming too soon
+    let db_result = await db.find_claim(address);
+    if (db_result) {
+      if (Number(db_result.last_claim)+CLAIM_FREQ > Date.now()) {
+        return await interaction.editReply("Error, your last claim was too soon! Run `/next_claim` to see when your next claim will be.");
+      }
+    }
+    //songbird enough balance
+    let enough_balance = await songbird.enough_balance(address, HOLDING_REQUIREMENT);
+    if (!enough_balance.success) {
+      return await interaction.editReply("Error, you do not hold enough SGB or WSGB.");
+    }
+    let token_tx_resp = await fetch("https://songbird-explorer.flare.network/api?module=account&action=tokentx&address="+address);
+    token_tx_resp = await token_tx_resp.json();
+    let aged_enough = await songbird.aged_enough(address, HOLDING_REQUIREMENT, token_tx_resp, enough_balance.wrapped_sgb_bal);
+    if (!aged_enough) {
+      let holds_aged_nft = await songbird.holds_aged_nfts(address, token_tx_resp);
+      //provide exemption if they hold aged nft
+      if (!holds_aged_nft) {
+        return await interaction.editReply(`Error, your SGB or WSGB needs to be held for at least 1 day (${songbird.HOLDING_BLOCK_TIME} blocks).`);
+      }
+    }
+    //send XAC, check for send error
+    let send_amount = db.get_amount();
+    let tx = await songbird.faucet_send_astral(user_info.address, send_amount);
+    if (!tx) {
+      return await interaction.editReply("Error, send failed! Probably gas issue, too many claims at once or faucet is out of funds. Try again in a few minutes.");
+    }
+    //add to db
+    await db.add_claim(user_info.address, send_amount);
+    //reply with embed that includes tx link
+    let faucet_embed = new discord.EmbedBuilder();
+    let month = db.get_month();
+    faucet_embed.setColor("#15d30e");
+    faucet_embed.setTitle("Faucet Claim");
+    faucet_embed.setURL("https://songbird-explorer.flare.network/tx/"+tx);
+    faucet_embed.setImage("https://cdn.discordapp.com/attachments/975616285075439636/1098738804904431686/XAC_check.gif");
+    faucet_embed.setDescription(`${send_amount} XAC has been sent to your address (\`${address}\`). You should receive it shortly! Come back in 24 hours to claim again.`);
+    faucet_embed.setTimestamp();
+    if (!db_result) {
+      faucet_embed.setFooter({ text: "Thanks! Note: user not found in DB." });
+    } else {
+      faucet_embed.setFooter({ text: "Thank you for participating in the XAC distribution!" });
+    }
+    return await interaction.editReply({ embeds: [faucet_embed] });
   }
 });
 
--- songbird.js
+++ songbird.js
@@ -8,7 +8,13 @@ const provider = new ethers.providers.JsonRpcProvider("https://songbird-api.flar
 let wallet = new ethers.Wallet(process.env.privkey);
 wallet = wallet.connect(provider);
 
+let faucet_wallet = new ethers.Wallet(process.env.faucet_privkey);
+faucet_wallet = faucet_wallet.connect(provider);
+
+//faucet wallet:
+
 const token_contract_address = "0x61b64c643fCCd6ff34Fc58C8ddff4579A89E2723";
+const nft_contract_address = "0x288F45e46aD434808c65880dCc2F21938b7Da23d";
 
 const erc20_abi = [
   {
@@ -50,14 +56,158 @@ const erc20_abi = [
   }
 ];
 
+const erc1155_abi = [
+  {
+		"inputs": [
+			{
+				"internalType": "address[]",
+				"name": "accounts",
+				"type": "address[]"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "ids",
+				"type": "uint256[]"
+			}
+		],
+		"name": "balanceOfBatch",
+		"outputs": [
+			{
+				"internalType": "uint256[]",
+				"name": "",
+				"type": "uint256[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
 let astral_token = new ethers.Contract(token_contract_address, erc20_abi, wallet);
+let astral_nft = new ethers.Contract(nft_contract_address, erc1155_abi, faucet_wallet);
+let wrapped_songbird_token = new ethers.Contract("0x02f0826ef6aD107Cfc861152B32B52fD11BaB9ED", erc20_abi, faucet_wallet);
+let faucet_astral_token = new ethers.Contract(token_contract_address, erc20_abi, faucet_wallet);
+
+//faucet requirements
+//how many blocks sgb/nft has to be held for. 43200 is 24 hours, since block time is 2 seconds so 43200 blocks is around 24 hours - 1800 blocks is around 1 Hour
+const HOLDING_BLOCK_TIME = 43200;
+
+//get songbird coin balance
+async function get_bal(address) {
+  return ethers.utils.formatEther(await provider.getBalance(address));
+}
+
+//check if they hold enough wrapped songbird or songbird to use the faucet
+async function enough_balance(address, holding_requirement) {
+  let wrapped_bal = await wrapped_songbird_token.balanceOf(address);
+  wrapped_bal = Number(ethers.utils.formatUnits(wrapped_bal.toString(), 18));
+  let reg_bal = Number(await get_bal(address));
+  return {
+    success: wrapped_bal >= holding_requirement || reg_bal >= holding_requirement,
+    wrapped_sgb_bal: wrapped_bal,
+    sgb_bal: reg_bal
+  };
+}
+
+async function aged_enough(address, holding_requirement, wrapped_songbird_resp, wrapped_sgb_bal) {
+  let holding_enough = false;
+  //get current block
+  let current_block = await provider.getBlockNumber();
+  let songbird_resp = await fetch("https://songbird-explorer.flare.network/api?module=account&action=eth_get_balance&address="+address+"&block="+String(current_block-HOLDING_BLOCK_TIME));
+  songbird_resp = await songbird_resp.json();
+  if (!songbird_resp.error) {
+    let songbird_snapshot = Number(ethers.utils.formatEther(songbird_resp.result));
+    if (songbird_snapshot >= holding_requirement) {
+      holding_enough = true;
+    }
+  }
+  if (wrapped_songbird_resp.result) {
+    wrapped_songbird_resp = wrapped_songbird_resp.result;
+    //timestamp attribute can also be used but whatever
+    //get token transfers within the hour and see if the (balance)-(total received)=(balance 24 hours ago) is above the holding req or not
+    wrapped_songbird_resp = wrapped_songbird_resp.filter(function(item) {
+      return item.tokenName === "Wrapped Songbird" && item.blockNumber >= current_block-HOLDING_BLOCK_TIME;
+    });
+    let wrapped_songbird_snapshot = 0;
+    for (let i=0; i < wrapped_songbird_resp.length; i++) {
+      if (wrapped_songbird_resp[i].to.toLowerCase() === address.toLowerCase()) {
+        wrapped_songbird_snapshot += Number(ethers.utils.formatUnits(wrapped_songbird_resp[i].value, 18));
+      } else {
+        wrapped_songbird_snapshot -= Number(ethers.utils.formatUnits(wrapped_songbird_resp[i].value, 18));
+      }
+    }
+    if ((wrapped_sgb_bal-wrapped_songbird_snapshot) >= holding_requirement) {
+      holding_enough = true;
+    }
+  }
+  if (holding_enough) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//checks if user has the right nfts, and has held them for at least 
+async function holds_aged_nfts(address, nft_resp) {
+  //genesis token id: 1
+  //hologram token id: 5
+  let nft_balances = await astral_nft.balanceOfBatch([address, address], [1, 5]);
+  let genesis_num = Number(nft_balances[0]);
+  let hologram_num = Number(nft_balances[1]);
+  if (genesis_num === 0 && hologram_num === 0) return false;
+  if (nft_resp.result) {
+    nft_resp = nft_resp.result;
+    //timestamp attribute can also be used but whatever
+    //get token transfers within the hour and see if the (balance)-(total received)=(balance 24 hours ago) is above the holding req or not
+    nft_resp = nft_resp.filter(function(item) {
+      return item.contractAddress.toLowerCase() === token_contract_address.toLowerCase() && (item.tokenID == "1" || item.tokenID == "2") && item.blockNumber >= current_block-HOLDING_BLOCK_TIME;
+    });
+    //net received genesis and hologram nfts during the period
+    let genesis_snapshot = 0;
+    let hologram_snapshot = 0;
+    for (let i=0; i < nft_resp.length; i++) {
+      if (nft_resp[i].to.toLowerCase() === address.toLowerCase()) {
+        if (nft_resp[i].tokenID == "1") {
+          genesis_snapshot += 1;
+        } else if (nft_resp[i].tokenID == "5") {
+          hologram_snapshot += 1;
+        }
+        wrapped_songbird_snapshot += Number(ethers.utils.formatUnits(nft_resp[i].value, 18));
+      } else {
+        if (nft_resp[i].tokenID == "1") {
+          genesis_snapshot -= 1;
+        } else if (nft_resp[i].tokenID == "5") {
+          hologram_snapshot -= 1;
+        }
+      }
+    }
+    if ((genesis_num-genesis_snapshot) >= 1 || (hologram_num-hologram_snapshot) >= 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 
 //send stuff, but also price queries and whatnot
 
 async function send_astral(address, amount) {
-  amount = ethers.utils.parseUnits(amount, 18);
+  amount = ethers.utils.parseUnits(String(amount), 18);
   try {
     return (await astral_token.transfer(address, amount)).hash;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+}
+
+async function faucet_send_astral(address, amount) {
+  amount = ethers.utils.parseUnits(String(amount), 18);
+  try {
+    return (await faucet_astral_token.transfer(address, amount, {
+      gasPrice: ethers.utils.parseUnits('70', 'gwei'),
+      gasLimit: 45000
+    })).hash;
   } catch (e) {
     console.log(e);
     return false;
@@ -94,8 +244,14 @@ async function get_historic() {
 }
 
 module.exports = {
+  HOLDING_BLOCK_TIME: HOLDING_BLOCK_TIME,
   get_liquidity_blaze: get_liquidity_blaze,
+  enough_balance: enough_balance,
+  get_bal: get_bal,
+  aged_enough: aged_enough,
+  holds_aged_nfts: holds_aged_nfts,
   send_astral: send_astral,
+  faucet_send_astral: faucet_send_astral,
   get_price: get_price,
   get_coin_price: get_coin_price,
   get_historic: get_historic,
